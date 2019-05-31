0d0f4c504df4e33ef42d2531f7495b5a4b37d310
Roshan Posakya <roshanposakya07@gmail.com>
2019-05-31 14:53:56 +0545

    Example

--- app/src/main/java/com/ar_tech/bearingdirection/MainActivity.java
+++ app/src/main/java/com/ar_tech/bearingdirection/MainActivity.java
@@ -10,29 +10,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        double lat1 = 39.099912;
-//        double lat2 = 38.627089;
-//        double lng1 = -94.581213;
-//        double lng2 = -90.200203;
+       double lat1 = 39.099912;
+       double lat2 = 38.627089;
+       double lng1 = -94.581213;
+       double lng2 = -90.200203;
 
-//         /*
-//
-//            determining the bearing angle
-//
-//         */
-//
-//        double bearing = DistanceBearingCalculation.bearing(lat1,lng1,lat2,lng2);
-//        System.out.println("Bearing : "+bearing);
-//
-//
-//         /*
-//
-//            determining the distance between two gps coordinates point
-//
-//         */
-//
-//        double distance = DistanceBearingCalculation.distance(lat1,lng1,lat2,lng2);
-//        System.out.println("Distance : "+distance);
+        /*
+
+           determining the bearing angle
+
+        */
+
+       double bearing = DistanceBearingCalculation.bearing(lat1,lng1,lat2,lng2);
+       System.out.println("Bearing : "+bearing);
+
+
+        /*
+
+           determining the distance between two gps coordinates point
+
+        */
+
+       double distance = DistanceBearingCalculation.distance(lat1,lng1,lat2,lng2);
+       System.out.println("Distance : "+distance);
 
 
     }
